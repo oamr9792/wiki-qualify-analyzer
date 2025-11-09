692ceab651e9cad7e3967b761fbb58c7f6dbbea7
oamr9792 <orani.amroussi@gmail.com>
2025-11-09 18:02:39 +0200

    fixes made

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: mode === 'development' ? {
-    host: '::',
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: `http://localhost:${process.env.DEV_API_PORT || 3005}`,
-        changeOrigin: true,
-        secure: false
+  server: mode === 'development'
+    ? {
+        host: '::',
+        port: 8080,
+        proxy: {
+          '/api': {
+            target: `http://localhost:${process.env.DEV_API_PORT || 3005}`,
+            changeOrigin: true,
+            secure: false
+          }
+        }
       }
-    }
-  } : undefined
+    : undefined
 }));
